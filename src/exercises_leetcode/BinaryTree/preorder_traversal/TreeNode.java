e6f8928e618c79358441aac686d2caacaddbeafe
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-11-09 17:03:43 -0300

    solving problems

--- src/exercises_leetcode/BinaryTree/preorder_traversal/TreeNode.java
+++ src/exercises_leetcode/BinaryTree/preorder_traversal/TreeNode.java
@@ -1,35 +1,35 @@
-package exercises_leetcode.BinaryTree.Binary_Tree_Preorder_Traversal;
+// package exercises_leetcode.BinaryTree.Binary_Tree_Preorder_Traversal;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
+// import java.util.ArrayList;
+// import java.util.List;
 
 
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}     TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;         this.left = left;
-         this.right = right;
-     }
- }
+
+
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode() {}     TreeNode(int val) { this.val = val; }
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;         this.left = left;
+//          this.right = right;
+//      }
+//  }
  
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        preorderHelper( root, result);
-        return result;
-    }
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> result = new ArrayList<>();
+//         preorderHelper( root, result);
+//         return result;
+//     }
     
-    private void preorderHelper(TreeNode node, List<Integer> result) {
-        //consultar se a raiz é vazia
-        if(node != null) {
-            result.add(node.val); // visita a raiz;
-            preorderHelper(node.left, result);
-            preorderHelper(node.right, result);
-        }
-    }
-}
+//     private void preorderHelper(TreeNode node, List<Integer> result) {
+//         //consultar se a raiz é vazia
+//         if(node != null) {
+//             result.add(node.val); // visita a raiz;
+//             preorderHelper(node.left, result);
+//             preorderHelper(node.right, result);
+//         }
+//     }
+// }
