bf4c73524c32861f8f94d99cea39ba19393bbbe1
rafaelnapolitanovescovi <rafaelnv.prof@gmail.com>
2025-02-05 12:47:41 -0300

    middle of linked list(not finished)

---
+++ src/exercises_leetcode/beggining/MiddleOfLinkedList/Solution.java
@@ -0,0 +1,24 @@
+//package exercises_leetcode.beggining.MiddleOfLinkedList;
+//
+//
+//public class Solution {
+//    public ListNode middleNode(ListNode head) {
+//
+//
+//        return ;
+//
+//    }
+//
+//}
+//
+//class ListNode {
+//    int value;
+//    ListNode nextPointer;
+//
+//    ListNode(){}
+//    ListNode(int value) {
+//        this.value = value;
+//    }
+//    ListNode(int value, ListNode nextPointer) {
+//        this.value = value; this.nextPointer = nextPointer;
+//    }}
